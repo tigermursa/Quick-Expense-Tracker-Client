14f1b4b4dbec412035872d3735b72f1e16d2d59f
Mursalin <mursalin2311@gmail.com>
2024-10-29 23:56:15 +0600

    installing redux for manign the all api call perfectly

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,12 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:4000",
+    credentials: "include",
+  }),
+  tagTypes: ["tasks"],
+
+  endpoints: () => ({}),
+});
