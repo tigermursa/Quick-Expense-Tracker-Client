4b93dc8696b05ef98190c5d5a76cea9c8e9f799b
Mursalin <mursalin2311@gmail.com>
2024-10-31 17:17:11 +0600

    added hosted backend url

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:4000",
+    baseUrl: "https://quick-expense-tracker-orpin.vercel.app",
     credentials: "include",
   }),
   tagTypes: ["data"],
