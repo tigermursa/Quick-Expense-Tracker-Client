fbc36e95b67ba32a8203e4e032d356e21045a170
Mursalin <mursalin2311@gmail.com>
2024-11-06 23:58:15 +0600

    updating code now

--- src/components/DataInputForm/DataInputForm.tsx
+++ src/components/DataInputForm/DataInputForm.tsx
@@ -56,7 +56,7 @@ const DataInputForm: React.FC = () => {
               htmlFor="name"
               className="block text-gray-200 font-semibold mb-2"
             >
-              Name
+              Names
             </label>
             <input
               type="text"
